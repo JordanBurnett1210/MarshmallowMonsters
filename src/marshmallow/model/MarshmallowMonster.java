77cf1f83bda9013aab844c6539e128e8965b60f1
JordanBurnett1210 <jabur1210@gmail.com>
2015-09-17 09:07:05 -0600

    Added a user input monster.

--- src/marshmallow/model/MarshmallowMonster.java
+++ src/marshmallow/model/MarshmallowMonster.java
@@ -25,9 +25,70 @@ public class MarshmallowMonster
 		this.monsterBellyButton = monsterBellyButton;
 	}
 	
+	//Getters & Setters
+	public String getMonsterName()
+	{
+		return monsterName;
+	}
+	
+	public int getMonsterEyes()
+	{
+		return monsterEyes;
+	}
+	
+	public int getMonsterNoses()
+	{
+		return monsterNoses;
+	}
+	
+	public double getMonsterHair()
+	{
+		return monsterHair;
+	}
+	
+	public double getMonsterLegs()
+	{
+		return monsterLegs;
+	}
+	
+	public boolean getMonsterBellyButton()
+	{
+		return monsterBellyButton;
+	}
+	
+	public void setMonsterName(String monsterName)
+	{
+		this.monsterName = monsterName;
+	}
+	
+	public void setMonsterEyes(int monsterEyes)
+	{
+		this.monsterEyes = monsterEyes;
+	}
+	
+	public void setMonsterNoses(int betterMonsterNoses)
+	{
+		this.monsterNoses = betterMonsterNoses;
+	}
+	
+	public void setMonsterHair(double monsterHair)
+	{
+		this.monsterHair = monsterHair;
+	}
+	
+	public void setMonsterLegs(double monsterLegs)
+	{
+		this.monsterLegs = monsterLegs;
+	}
+	
+	public void setMonsterBellyButton(boolean monsterBellyButton)
+	{
+		this.monsterBellyButton = monsterBellyButton;
+	}
+	
 	public String toString()
 	{
-		String monster = "This monster has " + monsterEyes + " eyes, it has " + monsterNoses + " noses, and it's name is " + monsterName + ".";
+		String monster = "This monster has " + monsterEyes + " eyes, it has " + monsterNoses + " noses, it has " + monsterLegs + " legs, it has "+ monsterHair + " hairs,and it's name is " + monsterName + ".";
 		
 		return monster;
 	}
--- src/marshmallows/controller/MonsterController.java
+++ src/marshmallows/controller/MonsterController.java
@@ -2,11 +2,14 @@ package marshmallows.controller;
 
 import marshmallow.model.MarshmallowMonster;
 import marshmallows.view.MarshmallowOutput;
+import java.util.Scanner;
 
 public class MonsterController
 {
 	private MarshmallowMonster jordanMonster;
+	private MarshmallowMonster userMonster;
 	private MarshmallowOutput myOutput;
+	private Scanner monsterScanner;
 	
 	public MonsterController()
 	{
@@ -17,21 +20,75 @@ public class MonsterController
 		boolean hasbellybutton =  true;
 		String name = "Harkonnen Splat";
 		
+		monsterScanner = new Scanner(System.in);
 		myOutput = new MarshmallowOutput();
 		jordanMonster = new MarshmallowMonster(name, eyes, noses, hair, legs, hasbellybutton); //must match constructor in Marshmallow.java
 	}
+	
 	public void start()
 	{
 		myOutput.displayMonsterInfo(jordanMonster.toString());
 		askQuestions();
-		myOutput.displayMonsterInfo("New Monster Info" + jordanMonster.toString());
+		myOutput.displayMonsterInfo("New Monster Info " + jordanMonster.toString());
 	}
 	
 	private void askQuestions()
 	{
+		System.out.println("Type in a better name for the monster.");
+		String betterMonsterName = monsterScanner.next();
+		jordanMonster.setMonsterName(betterMonsterName);
 		
+		System.out.println("Type in a different number of eyes for the monster.");
+		int betterMonsterEyes = monsterScanner.nextInt();
+		jordanMonster.setMonsterEyes(betterMonsterEyes);
+		
+		System.out.println("Type in a different number of noses for the monster.");
+		int betterMonsterNoses = monsterScanner.nextInt();
+		jordanMonster.setMonsterNoses(betterMonsterNoses);
+		
+		System.out.println("Type in a different number of hairs for the monster.");
+		double betterMonsterHair = monsterScanner.nextInt();
+		jordanMonster.setMonsterHair(betterMonsterHair);
+		
+		System.out.println("Type in a different number of legs for the monster.");
+		double betterMonsterLegs = monsterScanner.nextInt();
+		jordanMonster.setMonsterLegs(betterMonsterLegs);
+		
+		System.out.println("Type True if the monster has a bellybutton. Type false if it does not.");
+		boolean betterMonsterBellyButton = monsterScanner.nextBoolean();
+		jordanMonster.setMonsterBellyButton(betterMonsterBellyButton);
 	}
 
+	/**
+	 * This method will get the information and create an instance of a MarshmallowMonster.
+	 */
+	private void makeUserMonster()
 	
+	{
+		//Step one: Get variables.
+		String userName;  //Declare a String type called userName.
+		int userEyes;  //Declare a integer type called userEyes
+		int userNoses;
+		double userHair;
+		double userLegs;
+		boolean userBellyButton;
+		
+		//Step two: Define variables by using Scanner to get user input.
+		System.out.println("Type in your monster's name.");
+		userName = monsterScanner.nextLine();
+		System.out.println("Type in the number of eyes for your monster");
+		userEyes = monsterScanner.nextInt();
+		System.out.println("Type in the number of noses for your monster");
+		userNoses = monsterScanner.nextInt();
+		System.out.println("Type in the number of hairs for your monster");
+		userHair = monsterScanner.nextDouble();
+		System.out.println("Type in the number of legs for your monster");
+		userLegs = monsterScanner.nextDouble();
+		System.out.println("Does it have a bellybutton? Type true or false.");
+		userBellyButton = monsterScanner.nextBoolean();
+		
+		//Step three: Make a monster - use the Constructor!! Remember that the order of parameters matters!
+		userMonster = new MarshmallowMonster(userName, userEyes, userNoses, userHair, userLegs, userBellyButton);
+	}
 	
 }
